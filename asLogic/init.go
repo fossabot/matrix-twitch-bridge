0643c9b1068422e63b0a7f0c6f9f5f369767fd84
MTRNord <mtrnord1@gmail.com>
2018-04-01 21:41:51 +0200

    Add preparation for later Appservice implementation

---
+++ asLogic/init.go
@@ -0,0 +1,47 @@
+package asLogic
+
+import (
+	"maunium.net/go/mautrix-appservice-go"
+	"log"
+	"github.com/fatih/color"
+)
+
+var config *appservice.Config
+
+func Init()  {
+	var boldGreen = color.New(color.FgGreen).Add(color.Bold)
+	appservice.GenerateRegistration("twitch", "twitch", true, true)
+	boldGreen.Println("Please restart the Appservice with \"--config\"-flag applied")
+}
+
+func Run(cfgFile string)  {
+	config = appservice.Load(cfgFile)
+
+	queryHandler := QueryHandler{}
+
+	config.Init(queryHandler)
+
+	config.Listen()
+
+	for {
+		select {
+		case event := <-config.Events:
+			log.Println(event)
+		}
+	}
+}
+
+type QueryHandler struct {
+	users map[string]string
+	aliases map[string]string
+}
+
+func(q QueryHandler) QueryAlias(alias string) bool {
+	// TODO implement logic
+	return false
+}
+
+func(q QueryHandler) QueryUser(userID string) bool {
+	// TODO implement logic
+	return false
+}

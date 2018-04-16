fbfd1a65e6bc240ace83d6108f876e67925da3ae
MTRNord <mtrnord1@gmail.com>
2018-04-16 16:55:23 +0200

    Remove 2 letter shorthands as cobra does not support them

--- cmd/root.go
+++ cmd/root.go
@@ -53,12 +53,12 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().StringVarP(&util.CfgFile, "config", "c", "./config.yaml", "config file (default is ./config.yaml . It will get generated if no value is given)")
-	rootCmd.PersistentFlags().StringVarP(&util.DbFile, "database", "db", "./twitch.db", "db file where data gets saved/cached to (default is ./twitch.db .  It will get generated if no value is given)")
+	rootCmd.PersistentFlags().StringVar(&util.DbFile, "database", "./twitch.db", "db file where data gets saved/cached to (default is ./twitch.db .  It will get generated if no value is given)")
 	rootCmd.PersistentFlags().StringVar(&util.ClientID, "client_id", "", "client_id of the registered Twitch App")
 	rootCmd.PersistentFlags().StringVar(&util.ClientSecret, "client_secret", "", "client_secret of the registered Twitch App")
 	rootCmd.PersistentFlags().StringVar(&util.BotAToken, "bot_accessToken", "", "accessToken of the Twitch Bot User. You can aquire this by opening https://twitchapps.com/tmi and removing \"oauth:\" at the front")
 	rootCmd.PersistentFlags().StringVar(&util.BotUName, "bot_username", "", "username of the Twitch Bot User.")
-	rootCmd.PersistentFlags().StringVarP(&util.Publicaddress, "public_address", "pa", "", "Address of the Public Listening HTTP Server (used for the Twitch Callback)")
-	rootCmd.PersistentFlags().StringVarP(&util.TLSCert, "tls_cert", "tc", "", "Path to TLS Cert File.")
-	rootCmd.PersistentFlags().StringVarP(&util.TLSKey, "tls_key", "tk", "", "Path to TLS Key File.")
+	rootCmd.PersistentFlags().StringVar(&util.Publicaddress, "public_address", "", "Address of the Public Listening HTTP Server (used for the Twitch Callback)")
+	rootCmd.PersistentFlags().StringVar(&util.TLSCert, "tls_cert", "", "Path to TLS Cert File.")
+	rootCmd.PersistentFlags().StringVar(&util.TLSKey, "tls_key", "", "Path to TLS Key File.")
 }

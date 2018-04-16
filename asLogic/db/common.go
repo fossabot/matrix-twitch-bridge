303353f541823eebbaf266eecbf4996a33330051
MTRNord <mtrnord1@gmail.com>
2018-04-16 17:35:20 +0200

    We call init in the DB already only once in the code so no need of using sync.Once

--- asLogic/db/common.go
+++ asLogic/db/common.go
@@ -5,35 +5,31 @@ import (
 	"fmt"
 	"github.com/Nordgedanken/matrix-twitch-bridge/asLogic/util"
 	"log"
-	"sync"
 )
 
-var dbOnce sync.Once
 var db *sql.DB
 
 // Init prepares the DBby opening it and creating the required tables if needed
 func Init() (err error) {
-	dbOnce.Do(func() {
-		log.Println("Start setting up DB")
-		var openErr error
-		db, openErr = sql.Open("sqlite3", util.DbFile)
-		if openErr != nil {
-			err = openErr
-			return
-		}
+	log.Println("Start setting up DB")
+	var openErr error
+	db, openErr = sql.Open("sqlite3", util.DbFile)
+	if openErr != nil {
+		err = openErr
+		return
+	}
 
-		log.Println("Creating DB Tables if needed")
-		createTables := `CREATE TABLE IF NOT EXISTS users (id integer not null primary key, type text , mxid text, twitch_name text, twitch_token text, twitch_token_id text);
+	log.Println("Creating DB Tables if needed")
+	createTables := `CREATE TABLE IF NOT EXISTS users (id integer not null primary key, type text , mxid text, twitch_name text, twitch_token text, twitch_token_id text);
 						 CREATE TABLE IF NOT EXISTS tokens (id integer not null primary key, access_token text, token_type text, refresh_token text, expiry text);
 						 CREATE TABLE IF NOT EXISTS rooms (id integer not null primary key, room_alias text, room_id text, twitch_channel text);
 						 `
-		_, execErr := db.Exec(createTables)
-		if execErr != nil {
-			err = fmt.Errorf("DB EXEC ERR: %s", execErr)
-			return
-		}
-		log.Println("Finished setting DB Setup")
-	})
+	_, execErr := db.Exec(createTables)
+	if execErr != nil {
+		err = fmt.Errorf("DB EXEC ERR: %s", execErr)
+		return
+	}
+	log.Println("Finished setting DB Setup")
 	return
 }
 

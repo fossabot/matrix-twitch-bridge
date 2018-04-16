05828b135dfd8b6484acd8b7e2281de72c22e19a
MTRNord <mtrnord1@gmail.com>
2018-04-16 17:06:54 +0200

    Fix error handling on DB Init

--- asLogic/db/common.go
+++ asLogic/db/common.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/Nordgedanken/matrix-twitch-bridge/asLogic/util"
 	"sync"
 )
@@ -12,12 +13,20 @@ var db *sql.DB
 // Init prepares the DBby opening it and creating the required tables if needed
 func Init() (err error) {
 	dbOnce.Do(func() {
-		db, err = sql.Open("sqlite3", util.DbFile)
+		db, openErr := sql.Open("sqlite3", util.DbFile)
+		if openErr != nil {
+			err = openErr
+			return
+		}
 		createTables := `CREATE TABLE IF NOT EXISTS users (id integer not null primary key, type text , mxid text, twitch_name text, twitch_token text, twitch_token_id text);
 						 CREATE TABLE IF NOT EXISTS tokens (id integer not null primary key, access_token text, token_type text, refresh_token text, expiry text);
 						 CREATE TABLE IF NOT EXISTS rooms (id integer not null primary key, room_alias text, room_id text, twitch_channel text);
 						 `
-		_, err = db.Exec(createTables)
+		_, execErr := db.Exec(createTables)
+		if execErr != nil {
+			err = fmt.Errorf("DB EXEC ERR: %s", execErr)
+			return
+		}
 	})
 	return
 }

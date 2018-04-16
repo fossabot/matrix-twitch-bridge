f1737e7458c59e359fe8c19e166c3b50da4341ea
MTRNord <mtrnord1@gmail.com>
2018-04-16 18:25:47 +0200

    Debug Bot User ID

--- asLogic/db/users.go
+++ asLogic/db/users.go
@@ -255,6 +255,7 @@ func GetBotUser() (*user.BotUser, error) {
 
 	var localpart = strings.Replace(util.Config.Registration.Namespaces.UserIDs[0].Regex, ".+", util.Config.Registration.SenderLocalpart, -1)
 	var userID = "@" + localpart + ":" + util.Config.HomeserverDomain
+	util.Config.Log.Debugln("Bot UserID: ", userID)
 	botUser := &user.BotUser{
 		Mxid:        userID,
 		TwitchName:  util.BotUName,

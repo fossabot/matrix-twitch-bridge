4c6b0aed52a8d0bc6459d13fcc746ffa042b1729
MTRNord <mtrnord1@gmail.com>
2018-04-17 15:03:57 +0200

    Debug the raw error we get for further debug

--- asLogic/matrix_helper/users.go
+++ asLogic/matrix_helper/users.go
@@ -2,6 +2,7 @@ package matrix_helper
 
 import (
 	"fmt"
+	"github.com/Nordgedanken/matrix-twitch-bridge/asLogic/util"
 	"github.com/matrix-org/gomatrix"
 )
 
@@ -18,6 +19,7 @@ func CreateUser(client *gomatrix.Client, username string) error {
 	}
 
 	register, inter, err := client.Register(&registerReq)
+	util.Config.Log.Debugln("Wrapped Matrix Err: ", err.(gomatrix.HTTPError).WrappedError.(gomatrix.RespError).Err)
 	if err != nil && err.(gomatrix.HTTPError).WrappedError.(gomatrix.RespError).Err != "M_USER_IN_USE" {
 		return err
 	}

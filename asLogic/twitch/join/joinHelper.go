ce718487522c9c46df913bd839379bb6f1a3e34a
MTRNord <mtrnord1@gmail.com>
2018-04-17 14:16:09 +0200

    Leave Room first if needed as it might kill the WS otherwise

--- asLogic/twitch/join/joinHelper.go
+++ asLogic/twitch/join/joinHelper.go
@@ -4,7 +4,12 @@ package join
 import "github.com/gorilla/websocket"
 
 func Join(WS *websocket.Conn, channel string) error {
+	// Part Room
+	err := WS.WriteMessage(websocket.TextMessage, []byte("PART #"+channel))
+	if err != nil {
+		return err
+	}
 	// Join Room
-	err := WS.WriteMessage(websocket.TextMessage, []byte("JOIN #"+channel))
+	err = WS.WriteMessage(websocket.TextMessage, []byte("JOIN #"+channel))
 	return err
 }

da64cb49a1b94fa0c66079dcf2d1186fcf05d06f
MTRNord <mtrnord1@gmail.com>
2018-04-17 15:04:58 +0200

    Check against ErrCode

--- asLogic/matrix_helper/users.go
+++ asLogic/matrix_helper/users.go
@@ -19,11 +19,11 @@ func CreateUser(client *gomatrix.Client, username string) error {
 	}
 
 	register, inter, err := client.Register(&registerReq)
-	util.Config.Log.Debugln("Wrapped Matrix Err: ", err.(gomatrix.HTTPError).WrappedError.(gomatrix.RespError).Err)
-	if err != nil && err.(gomatrix.HTTPError).WrappedError.(gomatrix.RespError).Err != "M_USER_IN_USE" {
+	util.Config.Log.Debugln("Wrapped Matrix Err: ", err.(gomatrix.HTTPError).WrappedError.(gomatrix.RespError).ErrCode)
+	if err != nil && err.(gomatrix.HTTPError).WrappedError.(gomatrix.RespError).ErrCode != "M_USER_IN_USE" {
 		return err
 	}
-	if inter != nil || register == nil && err.(*gomatrix.HTTPError).WrappedError.(gomatrix.RespError).Err != "M_USER_IN_USE" {
+	if inter != nil || register == nil && err.(*gomatrix.HTTPError).WrappedError.(gomatrix.RespError).ErrCode != "M_USER_IN_USE" {
 		return fmt.Errorf("%s", "Error encountered during user registration")
 	}
 	return nil
